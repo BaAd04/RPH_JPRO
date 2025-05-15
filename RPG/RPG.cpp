cbfe3e0abb61bcbb82deba45663ade1d308693ad
BaAd04 <xbartoszx3@wp.pl>
2025-05-15 11:29:49 +0200

    Add files via upload

--- RPG/RPG.cpp
+++ RPG/RPG.cpp
@@ -1,8 +1,8 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 
 struct item
 {
@@ -58,6 +58,69 @@ struct postac {
 	int backpack_width;
 	};
 
+
+struct enemy {
+	std::string name;
+	int lvl;
+	int health;
+	int attack;
+	int mana;
+	int xpdrop;
+	int AS; // Attack Speed
+	int type;	//1 - normalny wrog, 2- boss
+};
+
+
+enemy* generate_enemy(int trapcount) {
+	enemy* en = (enemy*)malloc(sizeof enemy);
+	
+	int rand_enemytype = rand() % 10 + 1;
+	if (rand_enemytype > 6) {
+		en->name, "Goblin";
+		en->lvl = 1;
+		en->health = 3;
+		en->attack = 1;
+		en->mana = 0;
+		en->type = 1;
+		en->xpdrop = 5;
+		en->AS = 1;
+			
+	}
+	else if (rand_enemytype > 3) {
+		en->name, "Zombie";
+		en->lvl = 1;
+		en->health = 5;
+		en->attack = 2;
+		en->mana = 0;
+		en->type = 1;
+		en->xpdrop = 7;
+		en->AS = 1;
+	}
+	else if(rand_enemytype > 1) {
+		en->name, "Ork";
+		en->lvl = 2;
+		en->health = 8;
+		en->attack = 5;
+		en->mana = 0;
+		en->type = 2;
+		en->xpdrop = 10;
+
+	}
+	else if (rand_enemytype == 1) {
+		en->name, "Wizard";
+		en->lvl = 5;
+		en->health = 5;
+		en->attack = 8;
+		en->mana = 10;
+		en->type = 2;
+		en->xpdrop = 25;
+		en->AS = 4;
+	}
+	
+	return en;
+}
+
+
 postac* generate_postac() {
 	postac* post = (postac*)malloc(sizeof postac);
 	post->backpack_height = 10;
@@ -65,6 +128,7 @@ postac* generate_postac() {
 
 	post->posx = 1;
 	post->posy = 1;
+	post->xp = 0;
 
 	post->lvl = 1;
 	post->attack = rand() % 10 + 1;
@@ -147,81 +211,188 @@ int uderzenie(postac& post, item& wpn, item& armr) {
 	return real_hit_point > 0 ? real_hit_point : 0;
 
 }
+int trapcount = 0;
+int enemycount = 0;
+void generate_map(char** mapa, int mapx_size, int mapy_, postac* postac1) {
+	
+    for (int i = 0; i < mapx_size; i++) {
+        mapa[i] = (char*)malloc(sizeof(char) * mapy_);
+    }
 
-void generate_map() {
+    for (int i = 0; i < mapx_size; i++) {
+        for (int j = 0; j < mapy_; j++) {
+            int sciana_rand = rand() % 100 + 1;
+            int trap_rand = rand() % 100 + 1;
+            int enemy_rand = rand() % 100 + 1;
 
-	int mapx_size = 20;
-	int mapy_size = 10;
+            if (sciana_rand < 7) {
+                mapa[i][j] = '|';
+            } else if (trap_rand < 3) {
+                mapa[i][j] = 'T';
+				trapcount++;
+            } else if (enemy_rand < 2) {
+                mapa[i][j] = 'E';
+				enemycount++;
+            } else {
+                mapa[i][j] = '.';
+            }
+        }
+    }
+	int i = postac1->posx;
+	int j = postac1->posy;
+	mapa[i][j] = 'P'; // postac na mapie
+}
 
-	char **mapa = (char**)malloc(sizeof(char*) * mapx_size);
-	for (int i = 0; i < mapy_size; i++) {
-		mapa[i] = (char*)malloc(sizeof(char) * mapy_size);
-		}
+void show_map(char** mapa, int mapx_size, int mapy_size){
+	printf("\n\n\ttraps: %d enemies: %d\n", trapcount, enemycount);
+    printf("\n");
+    // Górna ramka
+    printf("\t");
+    for (int j = 0; j < mapy_size + 2; j++) {
+        printf("# ");
+    }
+    printf("\n");
 
-	for (int i = 0; i < mapx_size; i++) {
-		for (int j = 0; j < mapy_size; j++) {
-			int sciana_rand = rand() % 100 + 1;
-
-			int trap_rand = rand() % 100 + 1;
-			int enemy_rand = rand() % 100 + 1;
-
-			if (sciana_rand < 7) {
-				mapa[i][j] = 'r';
-			}
-			else if (trap_rand < 3) {
-				mapa[i][j] = 'T';
-			}
-			else if (enemy_rand < 2) {
-				mapa[i][j] = 'E';
+    // Środek mapy z ramkami bocznymi
+    for (int i = 0; i < mapx_size; i++) {
+        printf("\t# "); // Lewa ramka
+        for (int j = 0; j < mapy_size; j++) {
+			if (mapa[i][j] == 'T') {
+				printf(". "); // Pułapka ukryta 
 			}
 			else {
-				//mapa[i][j] = ' ';
+			printf("%c ", mapa[i][j]);
 			}
+            
+        }
+        printf("#\n"); // Prawa ramka
+    }
 
-			}
+    // Dolna ramka
+    printf("\t");
+    for (int j = 0; j < mapy_size + 2; j++) {
+        printf("# ");
+    }
+    printf("\n");
+}
+
+void widok_glowny(int mapx_size, int mapy_size, char** mapa, postac* postac1) {
+	// Wyświetlenie widoku głównego
+	printf("___________________________________________________________________________________\n");
+	printf("\tLVL: %d\tHP: %d\tSTM: %d\tXP: %d\t", postac1->lvl, postac1->health, postac1->stamina, postac1->xp);
+
+	show_map(mapa, mapx_size, mapy_size);
+	
+}
+void krok(char* move, char** mapa, postac* postac1, int mapx_size, int mapy_size) {
+
+
+			// Ruch w gore
+
+    if (move[0] == 'w') {
+        // Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
+        if (postac1->posx - 1 < 0) {
+            printf("Wychodzisz poza mape!\n");
+            return;
+        }
+        if (mapa[postac1->posx - 1][postac1->posy] == '|') {
+            printf("Wchodzisz w sciane!\n");
+			return;
+        }
+        if (mapa[postac1->posx - 1][postac1->posy] == 'T') {
+            printf("wpadasz na pułapke\n");
+			//pulapka
+        }
+            mapa[postac1->posx][postac1->posy] = '.';
+            postac1->posx--;
+            mapa[postac1->posx][postac1->posy] = 'P';
+        
+    }
+
+	//		ruch w dol
+
+	if (move[0] == 's') {
+		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
+		if (postac1->posx + 1 > mapx_size - 1) {
+			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx + 1][postac1->posy] == '|') {
+			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx + 1][postac1->posy] == 'T') {
+			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
+			//pulapka
+		}
+		mapa[postac1->posx][postac1->posy] = '.';
+		postac1->posx++;
+		mapa[postac1->posx][postac1->posy] = 'P';
+
+	}
+
+	//		ruch w lewo
+
+	if(move[0] == 'a') {
+		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
+		if (postac1->posy - 1 < 0) {
+			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx][postac1->posy - 1] == '|') {
+			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx][postac1->posy - 1] == 'T') {
+			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
+			//pulapka
+		}
+		mapa[postac1->posx][postac1->posy] = '.';
+		postac1->posy--;
+		mapa[postac1->posx][postac1->posy] = 'P';
+
+	}
+	if (move[0] == 'd') {
+		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
+		if (postac1->posy + 1 > mapy_size - 1) {
+			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx][postac1->posy + 1] == '|') {
+			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			return;
+		}
+		if (mapa[postac1->posx][postac1->posy + 1] == 'T') {
+			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
+			//pulapka
+		}
+		mapa[postac1->posx][postac1->posy] = '.';
+		postac1->posy++;
+		mapa[postac1->posx][postac1->posy] = 'P';
+
 	}
 
 
-	
-
-	
-
 }
-
-float equip(postac* postac1, item* new_ring = NULL) {
-
-	return 0;
-}
-
-
-
-
-
-
 
 
 int main()
 {
 	srand(time(NULL));
 
-	generate_map();
-	
+	int mapx_size = 10;
+	int mapy_size = 15;
 
-		
+	char** mapa = (char**)malloc(sizeof(char*) * mapx_size);
 
 	postac* postac1 = generate_postac();
-	std::cout << "Generated postac with hp: " << postac1->health << ", attack: " << postac1->attack << "\n";
 
+	generate_map(mapa, mapx_size, mapy_size, postac1);
 
-
-
-	char nazwa;
-	//std::cin >> nazwa;
-	item* pierscien22 = generate_ring('n');
-	printf("tu dziala");
-	postac1->backpack[0][0] = *pierscien22;
-	printf("Zawartosc plecaka: %s", postac1->backpack[0][0].name);
 	
+
+	item* pierscien22 = generate_ring('n');
+	postac1->backpack[0][0] = *pierscien22;
 
 	 //Generate a random weapon
 	
@@ -234,7 +405,7 @@ int main()
 	//std::cout << "Random armor generated with Armor Points: " << zbroja1.armorpoints << "\n";
 
 	item* pierscien = generate_ring('f');
-	std::cout << "Mana regen :" << pierscien->health_reg << "\n";
+	
 
 	postac1->right_hand.wpn = miecz1;
 	postac1->armor_slot.arm = zbroja1;
@@ -244,21 +415,18 @@ int main()
 	item* spareSword = generate_weapon();
 	postac1->ekwipunek = spareSword;
 	
-	//printf("Zawartosc plecaka:",);
+	while (postac1->health > 0) {
+
+		widok_glowny(mapx_size, mapy_size, mapa, postac1);
+		char move[1];
+		printf("Wybierz ruch (w/a/s/d): ");
+		scanf_s(" %c", &move[0]);
+				
+		krok(move, mapa, postac1, mapx_size, mapy_size);
+		
+
+	}
 	
-
-	printf ( "Szerokosc zbroi : %d oraz miecza %d", zbroja1->szerokosc, miecz1->szerokosc);
-
-	
-
-	
-
-
-
-	/*int damage = uderzenie(postac1, miecz1, zbroja1);
-	postac1.health = postac1.health - damage;
-	std::cout << "Damage dealt: " << damage << "\n  Healt left:" << postac1.health << "\n";
- */
 
 	free(postac1->ekwipunek);
 	
@@ -268,6 +436,11 @@ int main()
 	for (int i = 0; i < postac1->backpack_height; i++) {
 		free (postac1->backpack[i]) ;
 	}
+	for (int i = 0; i < mapx_size; i++) {
+		free(mapa[i]);
+	}
+	free(mapa);
+	
 
 	return 0;
 }
