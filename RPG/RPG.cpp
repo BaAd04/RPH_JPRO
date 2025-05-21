07831b4529d9faae7e387e6b58b9764cf880fecd
BaAd04 <xbartoszx3@wp.pl>
2025-05-21 18:52:43 +0200

    Add files via upload

--- RPG/RPG.cpp
+++ RPG/RPG.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstring>
+#include <windows.h>
+#include <conio.h>
 
 struct item
 {
@@ -56,6 +58,7 @@ struct postac {
 	item** backpack;
 	int backpack_height;
 	int backpack_width;
+	int effect; // 0 - normalny, 1- trucizna, 3 - głód 
 	};
 
 
@@ -68,10 +71,11 @@ struct enemy {
 	int xpdrop;
 	int AS; // Attack Speed
 	int type;	//1 - normalny wrog, 2- boss
+	int race; // 1 - goblin, 2 - zombie, 3 - ork, 4 - wizard
 };
 
 
-enemy* generate_enemy(int trapcount) {
+enemy* generate_enemy() {
 	enemy* en = (enemy*)malloc(sizeof enemy);
 	
 	int rand_enemytype = rand() % 10 + 1;
@@ -84,6 +88,7 @@ enemy* generate_enemy(int trapcount) {
 		en->type = 1;
 		en->xpdrop = 5;
 		en->AS = 1;
+		en->race = 1;	
 			
 	}
 	else if (rand_enemytype > 3) {
@@ -95,6 +100,7 @@ enemy* generate_enemy(int trapcount) {
 		en->type = 1;
 		en->xpdrop = 7;
 		en->AS = 1;
+		en->race = 2;
 	}
 	else if(rand_enemytype > 1) {
 		en->name, "Ork";
@@ -104,6 +110,7 @@ enemy* generate_enemy(int trapcount) {
 		en->mana = 0;
 		en->type = 2;
 		en->xpdrop = 10;
+		en->race = 3;
 
 	}
 	else if (rand_enemytype == 1) {
@@ -115,6 +122,7 @@ enemy* generate_enemy(int trapcount) {
 		en->type = 2;
 		en->xpdrop = 25;
 		en->AS = 4;
+		en->race = 4;
 	}
 	
 	return en;
@@ -129,11 +137,13 @@ postac* generate_postac() {
 	post->posx = 1;
 	post->posy = 1;
 	post->xp = 0;
+	post->effect = 0;
 
 	post->lvl = 1;
 	post->attack = rand() % 10 + 1;
-	post->health = rand() % 10 + 1;
+	post->health = rand() % 20 + 10;
 	post->stamina = rand() % 15 + 6;
+	post->speed = rand() % 12 + 3;
 	
 	int size = 100;
 	
@@ -144,12 +154,143 @@ postac* generate_postac() {
 		post->backpack[i] = (item*)malloc(sizeof(item) * post->backpack_width);
 	}
 
-
-	
-
 	return post;
 }
+//void walka(postac* postac1) {
 
+//	printf("\n\n\t[WALKA Z WROGIEM!]\n\n");
+//}
+
+void ekran_walki(postac* postac1) {
+	enemy* wrog = generate_enemy();
+	//while(wrog->health > 0 && postac1->health >0) {
+	system("cls");
+
+	switch (wrog->race) {
+	case 1:
+		printf(
+				"										 \n"
+				"						ENEMY	|	HERO \n"
+				"							|\n"				
+				"	 	    ⣠⣶⣿⣿⣶⣄⠀⠀⠀⠀⠀	⠀⠀⠀⠀⠀	⠀⠀		|\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀	\tHP: %d	|	%d\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢧⡀⠀⠀⠀⠀⠀⠀⠀		⠀	|\n"
+				"	⠀⠢⣤⣀⡀⠀⠀⠀⢿⣧⣄⡉⠻⢿⣿⣿⡿⠟⢉⣠⣼⡿⠀⠀⠀⠀⣀⣤⠔⠀			|\n"
+				"	⠀⠀⠈⢻⣿⣶⠀⣷⠀⠉⠛⠿⠶⡴⢿⡿⢦⠶⠿⠛⠉⠀⣾⠀⣶⣿⡟⠁⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠻⣿⡆⠘⡇⠘⠷⠠⠦⠀⣾⣷⠀⠴⠄⠾⠃⢸⠃⢰⣿⠟⠀⠀⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠀⠋⢠⣾⣥⣴⣶⣶⣆⠘⣿⣿⠃⣰⣶⣶⣦⣬⣷⡄⠙⠀⠀⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠀⠀⢋⠛⠻⠿⣿⠟⢹⣆⠸⠇⣰⡏⠻⣿⠿⠟⠛⡙⠀⠀⠀⠀⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠀⠀⠈⢧⡀⠠⠄⠀⠈⠛⠀⠀⠛⠁⠀⠠⠄⢀⡼⠁⠀⠀⠀⠀⠀		⠀	|\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠈⢻⣦⡀⠃⠀⣿⡆⢰⣿⠀⠘⢀⣴⡟⠁⠀⠀⠀⠀⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣦⡀⠘⠇⠸⠃⢀⣴⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀			|\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣷⣄⣠⣾⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀		⠀	|\n"
+				"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀		⠀	|\n"
+			"        \n"
+			"        \n"
+			"        \n"
+			"	 ____       _     _ _		⠀\n"
+			"	/ ___ | ___ | |__ | (_)_ __	⠀\n"
+			"	| |  _ / _ ⧵ | '_ ⧵| | | '_ ⧵	⠀\n"
+			"	| |_| | (_) | |_) | | | | | |		⠀\n"
+			"	⧵____ | ⧵___/|_.__/|_ | _ | _	⠀\n",wrog->health, postac1->health
+		);
+		break;
+	case 2:
+		printf(
+			"	        \n"
+			"									ENEMY	|	HERP \n"
+			"										|\n"		
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠖⠊⠉⠉⠉⠉⢉⠝⠉⠓⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⢀⡴⣋⠀⠀⣤⣒⡠⢀⠀⠐⠂⠀⠤⠤⠈⠓⢦⡀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⣰⢋⢬⠀⡄⣀⠤⠄⠀⠓⢧⠐⠥⢃⣴⠤⣤⠀⢀⡙⣆⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⢠⡣⢨⠁⡘⠉⠀⢀⣤⡀⠀⢸⠀⢀⡏⠑⠢⣈⠦⠃⠦⡘⡆⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⢸⡠⠊⠀⣇⠀⠀⢿⣿⠇⠀⡼⠀⢸⡀⠠⣶⡎⠳⣸⡠⠃⡇⠀⠀⠀			\t\t\t|\n"
+			"	⢀⠔⠒⠢⢜⡆⡆⠀⢿⢦⣤⠖⠒⢂⣽⢁⢀⠸⣿⣦⡀⢀⡼⠁⠀⠀⡇⠒⠑⡆			\t\t\t|\n"
+			"	⡇⠀⠐⠰⢦⠱⡤⠀⠈⠑⠪⢭⠩⠕⢁⣾⢸⣧⠙⡯⣿⠏⠠⡌⠁⡼⢣⠁⡜⠁			\t\t\t|\n"
+			"	⠈⠉⠻⡜⠚⢀⡏⠢⢆⠀⠀⢠⡆⠀⠀⣀⣀⣀⡀⠀⠀⠀⠀⣼⠾⢬⣹⡾⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠉⠀⠉⠀⠀⠈⣇⠀⠀⠀⣴⡟⢣⣀⡔⡭⣳⡈⠃⣼⠀⠀⠀⣼⣧⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀⠀⣸⣿⣿⣿⡿⣷⣿⣿⣷⠀⡇⠀⠀⠀⠙⠊⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣠⠀⢻⠛⠭⢏⣑⣛⣙⣛⠏⠀⡇⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠠⠜⠓⠉⠉⠀⠐⢒⡒⡍⠐⡇⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠢⠤⣀⣀⣀⣀⣘⠧⠤⠞⠁⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"        \n"
+			"        \n"
+			"        \n"
+			"	   _____               _     _			\n"
+			"	   |__  /___  _ __ ___ | |__ (_) ___	\n"
+			"	    / // _ ⧵| '_ ` _ ⧵| '_ ⧵| |/ _ ⧵	\n"
+			"	   / /| (_) | | | | | | |_) | |  __/	\n"
+			"	  /____⧵___/|_| |_| |_|_.__/|_|⧵___|	\n");
+		
+		break;
+	case 3:
+		printf(
+			"	        \n"
+			"									ENEMY	|	HERP \n"
+			"										|\n"
+			"	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣧⣄⣉⣉⣠⣼⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 		    \t\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⡿⣿⣿⣿⣿⢿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⣼⣤⣤⣈⠙⠳⢄⣉⣋⡡⠞⠋⣁⣤⣤⣧⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⢲⣶⣤⣄⡀⢀⣿⣄⠙⠿⣿⣦⣤⡿⢿⣤⣴⣿⠿⠋⣠⣿⠀⢀⣠⣤⣶⡖⠀			\t\t\t|\n"
+			"	⠀⠀⠙⣿⠛⠇⢸⣿⣿⡟⠀⡄⢉⠉⢀⡀⠉⡉⢠⠀⢻⣿⣿⡇⠸⠛⣿⠋⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠘⣷⠀⢸⡏⠻⣿⣤⣤⠂⣠⣿⣿⣄⠑⣤⣤⣿⠟⢹⡇⠀⣾⠃⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠘⠀⢸⣿⡀⢀⠙⠻⢦⣌⣉⣉⣡⡴⠟⠋⡀⢀⣿⡇⠀⠃⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⢸⣿⣧⠈⠛⠂⠀⠉⠛⠛⠉⠀⠐⠛⠁⣼⣿⡇⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠸⣏⠀⣤⡶⠖⠛⠋⠉⠉⠙⠛⠲⢶⣤⠀⣹⠇⠀⠀⠀⠀⠀	⠀		\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣶⣿⣿⣿⣿⣿⣿⣶⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠛⠛⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀			\t\t\t|\n"
+			"	        \n"
+			"	        \n"
+			"	        \n"
+			"		  ___       _				\n"
+			"		 / _ ⧵ _ __| | __			\n"
+			"		| | | | '__| |/ /			\n"
+			"		| |_| | |  |    <			\n"
+			"		 ⧵___/|_|  |_ |⧵_⧵			\n"
+		);
+		break;
+	case 4:
+		printf(
+												
+													
+			"			     _,-'|				|\n"		
+			"			 ,-' ._  |		ENEMY	|	HERP \n"	
+			"		 . || ,   |####⧵ |			|\n"
+			"		 ⧵.`', /  ⧵####| |			|\n"
+			"	   	 =, . =   | ###| |			|\n"
+			"		 / || ⧵ , -'⧵#/,'`.			|\n"
+			"		   || ,  '   `,,. `.			|\n"
+			"		  , | ____, ' , ,;' ⧵| |		|\n"
+			"		 (3 | ⧵    _/|/'   _| |		\n"
+			"		   ||/, -'' | >-'' _, ⧵⧵		\n"
+			"	           || '      ==⧵ ,-', '		    \n"
+			"		   ||       |  V ⧵, |		    \n"
+			"		   ||       |    |` |		\n"
+			"		   ||       |    |   ⧵		\n"
+			"		   ||       |    ⧵    ⧵		\n"
+			"		   ||       |     |    ⧵		\n"
+			"		   ||       |      ⧵_,-'		\n"
+			"		   ||       |___,,--')_⧵		\n"
+			"		   ||         |_|   ccc/		\n"
+			"		   ||        ccc/			\n"
+			"		   ||						\n"
+			"	      ⧵ ⧵      / (_)______ _ _ __ __| |\n"
+			"	       ⧵ ⧵ /⧵ / /| |_  / _` | '__/ _` |\n"
+			"		⧵ V  V / | |/ / (_| | | | (_| |	\n"
+			"		 ⧵_/⧵_/  |_/___⧵__,_|_|  ⧵__,_| \n"
+
+											);
+		break;
+	default:
+		break;
+	}
+	Sleep(1000);
+	//walka(postac1);
+	//}
+
+}
 item* generate_ring(char nazwa) {
 
 	
@@ -205,10 +346,57 @@ item* generate_armor() {
 
 
 }
-int uderzenie(postac& post, item& wpn, item& armr) {
-	int real_hit_point = (post.attack + 2 * wpn.AP) - armr.armorpoints;
+//int uderzenie(postac& post, item& wpn, item& armr) {
+//	int real_hit_point = (post.attack + 2 * wpn.AP) - armr.armorpoints;
+//
+//	return real_hit_point > 0 ? real_hit_point : 0;//}
 
-	return real_hit_point > 0 ? real_hit_point : 0;
+
+void pulapka(postac* postac1) {			//healt attack stamina speed
+	int trap_rand = rand() % 100 + 1;
+	int trap_type;
+	int trap_damage;
+	if (trap_rand < 26) {
+		trap_type = 1;		//health trap
+
+	}
+	else if (trap_rand < 51) {
+		trap_type = 2;		//attack trap
+	}
+	else if (trap_rand < 76) {
+		trap_type = 3;		//stamina trap
+	}
+	else if (trap_rand < 101) {
+		trap_type = 4;		//speed trap
+	}
+	switch (trap_type) {
+	case 1:
+		trap_damage = rand() % 3 + 1;
+		postac1->health -= trap_damage;
+		printf("\n\t[WPADLES W PULAPKE DAMAGE!]\n\n");
+		printf("\t[PULAPKA ZABIERA CI %d HP]\n", trap_damage);
+		break;
+	case 2:
+		trap_damage = rand() % 3 + 1;		
+		postac1->attack -= trap_damage;
+		printf("\n\t[WPADLES W PULAPKE REDUKCJI ATAKU!]\n\n");
+		printf("\t[PULAPKA ZABIERA CI %d ATAKU]\n", trap_damage);
+		break;
+	case 3:
+		trap_damage = rand() % 3 + 1;
+		postac1->stamina -= trap_damage;
+		printf("\n\t[WPADLES W PULAPKE REDUKCJI STAMINY!]\n\n");
+		printf("\t[PULAPKA ZABIERA CI %d STAMINY]\n", trap_damage);
+		break;
+	case 4:
+		trap_damage = rand() % 2 + 1;
+		postac1->speed -= trap_damage;
+		printf("\n\t[WPADLES W PULAPKE REDUKCJI PREDKOSCI!]\n\n");
+		printf("\t[PULAPKA ZABIERA CI %d PREDKOSCI]\n", trap_damage);
+		break;
+	default:
+		break;
+	}
 
 }
 int trapcount = 0;
@@ -240,6 +428,12 @@ void generate_map(char** mapa, int mapx_size, int mapy_, postac* postac1) {
     }
 	int i = postac1->posx;
 	int j = postac1->posy;
+	if(mapa[i][j] == 'T') {
+		trapcount--;
+	}
+	if (mapa[i][j] == 'E') {
+		enemycount--;	
+	}
 	mapa[i][j] = 'P'; // postac na mapie
 }
 
@@ -275,34 +469,76 @@ void show_map(char** mapa, int mapx_size, int mapy_size){
     }
     printf("\n");
 }
-
-void widok_glowny(int mapx_size, int mapy_size, char** mapa, postac* postac1) {
-	// Wyświetlenie widoku głównego
-	printf("___________________________________________________________________________________\n");
-	printf("\tLVL: %d\tHP: %d\tSTM: %d\tXP: %d\t", postac1->lvl, postac1->health, postac1->stamina, postac1->xp);
-
-	show_map(mapa, mapx_size, mapy_size);
-	
+void model_hero(postac* postac1) {					//model asci bedzie sie roznil w zaleznosci od broni
+	printf(
+		"		       {}				\n"
+		"		      .--.				\n"
+		"		     /.--.⧵			\n"
+		"		    |= == =|				\n"
+		"		    | `::` |			\n"
+		"		  .-;`⧵..../ `;-.		\n"
+		"		  /  |...::... |  ⧵		\n"
+		"		  |  /''': :'''⧵   |	\n"
+		"		  ; --'⧵   ::  /⧵--;	\n"
+		"	         <__>,>._::_. <,<__>	\n"
+		"	       |  |/   ^^    ⧵|  |	\n"
+		"	       ⧵::/|         |⧵::/	\n"
+		"	       |||⧵|         |/|||	\n"
+		"	       ''' |___/ ⧵___| '''	\n"
+		"                   ⧵_ |  | _ /			\n"
+		"	            <_ >< _>				\n"
+		"	            |  ||  |					\n"
+		"	            |  ||  |					\n"
+		"	           _⧵.:||:./_			\n"
+		"		  /____/ ⧵____⧵		\n\n\n"
+		"_____________________________________________________________________________________________________________\n");
 }
+void pokaz_postac(postac* postac1) {
+	system("cls");
+	printf("\n"
+		"\t _   _ _____ ____   ___\n"
+		"\t| | | | ____ | _ ⧵ / _	⧵	  \n"
+		"\t| |_| |  _|  ||_) | | | |\n"
+		"\t|  _  | |___ | _ <| |_| |\n"
+		"\t|_| |_| _____|_|⧵_⧵⧵___/ \n\n\n");
+
+	model_hero(postac1);
+}
+
+
+void widok_glowny(int mapx_size, int mapy_size, char** mapa, postac* postac1, int runda) {
+	
+    printf("_____________________________________________________________________________________________________________\n");
+    printf("\tRunda = %d LVL: %d\tHP: %d\tSTM: %d\tXP: %d\t AT: %d\t SPD: %.1f\t", runda, postac1->lvl, postac1->health, postac1->stamina, postac1->xp, postac1->attack, postac1->speed);
+    show_map(mapa, mapx_size, mapy_size);
+}
+
 void krok(char* move, char** mapa, postac* postac1, int mapx_size, int mapy_size) {
 
 
 			// Ruch w gore
-
+	system("cls");
     if (move[0] == 'w') {
+		
         // Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
         if (postac1->posx - 1 < 0) {
-            printf("Wychodzisz poza mape!\n");
+			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
             return;
         }
         if (mapa[postac1->posx - 1][postac1->posy] == '|') {
-            printf("Wchodzisz w sciane!\n");
+			printf("\n\t[WCHODZISZ W SCIANE!]\n\n");
 			return;
         }
         if (mapa[postac1->posx - 1][postac1->posy] == 'T') {
-            printf("wpadasz na pułapke\n");
-			//pulapka
+			printf("\n\t[WPADASZ W PULAPKE!]\n\n");
+			trapcount--;
+			pulapka(postac1);
         }
+		if (mapa[postac1->posx - 1][postac1->posy] == 'E') {
+			printf("\n\t[WPADASZ NA WROGA!]\n\n");
+			enemycount--;
+			ekran_walki(postac1);
+		}
             mapa[postac1->posx][postac1->posy] = '.';
             postac1->posx--;
             mapa[postac1->posx][postac1->posy] = 'P';
@@ -312,18 +548,25 @@ void krok(char* move, char** mapa, postac* postac1, int mapx_size, int mapy_size
 	//		ruch w dol
 
 	if (move[0] == 's') {
+		
 		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
 		if (postac1->posx + 1 > mapx_size - 1) {
 			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx + 1][postac1->posy] == '|') {
-			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			printf("\n\t[WCHODZISZ W SCIANE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx + 1][postac1->posy] == 'T') {
-			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
-			//pulapka
+			printf("\n\t[WPADASZ W PULAPKE!]\n\n");
+			trapcount--;
+			pulapka(postac1);
+		}
+		if (mapa[postac1->posx + 1][postac1->posy] == 'E') {
+			printf("\n\t[WPADASZ NA WROGA!]\n\n");
+			enemycount--;
+			ekran_walki(postac1);
 		}
 		mapa[postac1->posx][postac1->posy] = '.';
 		postac1->posx++;
@@ -334,18 +577,25 @@ void krok(char* move, char** mapa, postac* postac1, int mapx_size, int mapy_size
 	//		ruch w lewo
 
 	if(move[0] == 'a') {
+		
 		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
 		if (postac1->posy - 1 < 0) {
 			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx][postac1->posy - 1] == '|') {
-			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			printf("\n\t[WCHODZISZ W SCIANE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx][postac1->posy - 1] == 'T') {
-			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
-			//pulapka
+			printf("\n\t[WPADASZ W PULAPKE!]\n\n");
+			trapcount--;
+			pulapka(postac1);
+		}
+		if (mapa[postac1->posx][postac1->posy - 1] == 'E') {
+			printf("\n\t[WPADASZ NA WROGA!]\n\n");
+			enemycount--;
+			ekran_walki(postac1);
 		}
 		mapa[postac1->posx][postac1->posy] = '.';
 		postac1->posy--;
@@ -353,33 +603,44 @@ void krok(char* move, char** mapa, postac* postac1, int mapx_size, int mapy_size
 
 	}
 	if (move[0] == 'd') {
+		
 		// Sprawdzenie, czy nie wychodzimy poza górną krawędź mapy
 		if (postac1->posy + 1 > mapy_size - 1) {
 			printf("\n\t[WYCHODZISZ POZA MAPE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx][postac1->posy + 1] == '|') {
-			printf("\n\t[WCHODZISZ W ŚCIANE!]\n\n");
+			printf("\n\t[WCHODZISZ W SCIANE!]\n\n");
 			return;
 		}
 		if (mapa[postac1->posx][postac1->posy + 1] == 'T') {
-			printf("\n\t[WPADASZ W PUŁAPKĘ!]\n\n");
-			//pulapka
+			printf("\n\t[WPADASZ W PULAPKE!]\n\n");
+			trapcount--;
+			pulapka(postac1);
+		}
+		if (mapa[postac1->posx][postac1->posy + 1] == 'E') {
+			printf("\n\t[WPADASZ NA WROGA!]\n\n");
+			enemycount--;
+			ekran_walki(postac1);
 		}
 		mapa[postac1->posx][postac1->posy] = '.';
 		postac1->posy++;
 		mapa[postac1->posx][postac1->posy] = 'P';
 
 	}
-
+	
 
 }
 
 
 int main()
-{
-	srand(time(NULL));
 
+	
+{
+	
+	SetConsoleOutputCP(CP_UTF8);
+	srand(time(NULL));
+	int stage = 0;	//0- mapa, 1- ekwipunek
 	int mapx_size = 10;
 	int mapy_size = 15;
 
@@ -414,17 +675,33 @@ int main()
 	
 	item* spareSword = generate_weapon();
 	postac1->ekwipunek = spareSword;
-	
-	while (postac1->health > 0) {
+	int runda = 1;
 
-		widok_glowny(mapx_size, mapy_size, mapa, postac1);
+	while (postac1->health > 0) {
+		
+
+		widok_glowny(mapx_size, mapy_size, mapa, postac1, runda);
 		char move[1];
 		printf("Wybierz ruch (w/a/s/d): ");
 		scanf_s(" %c", &move[0]);
 				
+		if(move[0] == 'e') {
+			char znak;
+			do {
+				pokaz_postac(postac1);
+				znak = _getch();
+			} while(znak != 'q');
+			system("cls");
+			continue;
+		}		 
+					
 		krok(move, mapa, postac1, mapx_size, mapy_size);
-		
 
+		//enemy_check()		//sprawdza czy wrog jest w poblizu i podaje jego dane
+
+		//no_enemy_check()	//jesli na mapie nie ma wroga to tworzy go 
+		
+		runda++;
 	}
 	
 
